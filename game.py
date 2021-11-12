d8098d6f6d3bf71ccb9d52ffed85292c50818b85
James Watson <56607452+jedben2@users.noreply.github.com>
2021-11-12 10:35:17 +0000

    finished game

--- game.py
+++ game.py
@@ -3,6 +3,8 @@
 # if time out (5 seconds) them X appear (make new round happen)
 # need make number of rounds and then total points. maube highscore perhaps not since is prototype
 
+points = 0
+
 from microbit import *
 
 compass_directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
@@ -21,9 +23,54 @@ def draw_alien():
         # # . # #
         """)
 
+def die():
+    global points
+    basic.show_leds("""
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_string("Points: " + str(points))
+    basic.clear_screen()
+    basic.show_string("Bye!")
+
+def hit():
+    global points
+    basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    """)
+    pause(50)
+    basic.show_leds("""
+    . . . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    . . . . .
+    """)
+    pause(50)
+    basic.show_leds("""
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    """)
+    pause(50)
+    points += 1
+
 def main():
     playing = True
+    interval = 10
     while playing:
+        if interval > 3: interval -= 1
         alien = randint(0, 360) + 180
         if alien > 360: alien -= 360
         if input.button_is_pressed(Button.AB):
@@ -36,27 +83,41 @@ def main():
             if 22.5 <= alien < 337.5:
                 if alien - 22.5 < input.compass_heading() < alien + 22.5:
                     draw_alien()
-                else: 
-                    basic.show_number(int(5 - (input.running_time() - t1) // 1000), 1)
+                else:
+                    basic.show_number(int(interval - (input.running_time() - t1) // 1000), 1)
             elif alien > 375.5:
                 if alien - 22.5 < input.compass_heading() or 22.5 - 360 + alien > input.compass_heading():
                     draw_alien()
-                else: 
-                    basic.show_number(int(5 - (input.running_time() - t1) // 1000), 1)
+                else:
+                    basic.show_number(int(interval - (input.running_time() - t1) // 1000), 1)
             else:
                 if alien - 22.5 + 360 < input.compass_heading() or alien + 22.5 > input.compass_heading():
                     draw_alien()
                 else:
-                    basic.show_number(int(5 - (input.running_time() - t1) // 1000), 1)
-            if input.running_time() - t1 > 6000:
-                basic.show_leds("""
-                # . . . #
-                . # . # .
-                . . # . .
-                . # . # .
-                # . . . #
-                """)
-                pause(2000)
+                    basic.show_number(int(interval - (input.running_time() - t1) // 1000), 1)
+            if input.running_time() - t1 > interval * 1000 + 1000:
+                die()
+                playing = False
+                break
+        basic.clear_screen()
+        if 22.5 <= alien < 337.5:
+            if alien - 22.5 < input.compass_heading() < alien + 22.5:
+                hit()
+            else:
+                die()
+                playing = False
+        elif alien > 375.5:
+            if alien - 22.5 < input.compass_heading() or 22.5 - 360 + alien > input.compass_heading():
+                hit()
+            else:
+                die()
+                playing = False
+        else:
+            if alien - 22.5 + 360 < input.compass_heading() or alien + 22.5 > input.compass_heading():
+                hit()
+            else:
+                die()
+                playing = False
         basic.clear_screen()
 
 main()
